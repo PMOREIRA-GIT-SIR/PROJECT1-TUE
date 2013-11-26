846973bc5e5c760573d8886f05f24c49e40ed4c0
Pedro Moreira <pmoreira.estg@gmail.com>
2013-11-26 16:09:17 +0000

    fim da aula de 26.11.2013

--- KeyServerJSON.php
+++ KeyServerJSON.php
@@ -1,6 +1,10 @@
 <?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 include_once 'CKeyGen.php';
 //header("Content-Type: text/xml");
 $kg = new CKeyGen();
+sleep(3);
 echo $kg->KeyAsJSON();
 ?>
